28ef1e3f2e9888305a518a640f1c98c791e45a61
lisaprincessbabycakes <yt2972@nyu.edu>
2024-10-14 14:33:35 -0400

    Update sketch.js

--- HW05B /sketch.js
+++ HW05B /sketch.js
@@ -12,7 +12,6 @@ function draw() {
   background(220,0,200);
  
   
-  // Draw all the circles
   for (let circle of circles) {
     fill(0, 255, 0);
     ellipse(circle.x, circle.y, 10, 10); 
@@ -22,7 +21,6 @@ function draw() {
 }
 
 function mousePressed() {
-  // Add a new circle at the mouse position
   circles.push(createVector(mouseX, mouseY));
 
  
